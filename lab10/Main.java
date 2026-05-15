6b7503bc43cbeb301ae1f59db36f6b37fcc6ac8c
b251940089 <b251940089@erdenetis.edu.mn>
2026-05-15 16:49:49 +0800

    Update Main.java

--- lab10/Main.java
+++ lab10/Main.java
@@ -1,5 +1,31 @@
 package lab10;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Ovog: ");
+		String surname = sc.nextLine();
+		System.out.print("Ner: ");
+		String name = sc.nextLine();
+		System.out.print("Reg no: ");
+		String regNo = sc.nextLine();
+		System.out.print("Torson on (yyyy-mm-dd): ");
+		String birthDate = sc.nextLine();
+		System.out.print("Worker code: ");
+		String workerCode = sc.nextLine();
+		System.out.print("Branch: ");
+		String branchName = sc.nextLine();
+		System.out.print("Ajild orson ogno (yyyy-mm-dd): ");
+		String startDate = sc.nextLine();
+
+		System.out.println();
+
+		worker w = new worker(surname, name, regNo, birthDate, workerCode, branchName, startDate);
+		w.info();
+
+		sc.close();
+	}
 }
