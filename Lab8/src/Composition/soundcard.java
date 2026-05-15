901fbf847a387164c01be93af497170984ad1878
b251940089 <b251940089@erdenetis.edu.mn>
2026-05-15 16:43:31 +0800

    Update soundcard.java

--- Lab8/src/Composition/soundcard.java
+++ Lab8/src/Composition/soundcard.java
@@ -1,5 +1,23 @@
 package Composition;
 
-public class soundcard {
+public class soundCard {
+	private int channels;
+	private int sampleRate;
+	private String brand;
+	private String soundInterface;
 
+	public soundCard(int channels, int sampleRate, String brand, String soundInterface) {
+		this.channels = channels;
+		this.sampleRate = sampleRate;
+		this.brand = brand;
+		this.soundInterface = soundInterface;
+	}
+
+	public void printInfo() {
+		System.out.println("-- Sound Card --");
+		System.out.println("Brand: " + brand);
+		System.out.println("Channels: " + channels);
+		System.out.println("Sample Rate: " + sampleRate + " Hz");
+		System.out.println("Interface: " + soundInterface);
+	}
 }
