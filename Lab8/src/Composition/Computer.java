9a8cedad8af99348bd857f8ed8c65faa5f4e8b56
b251940089 <b251940089@erdenetis.edu.mn>
2026-05-15 16:42:09 +0800

    Update Computer.java

--- Lab8/src/Composition/Computer.java
+++ Lab8/src/Composition/Computer.java
@@ -1,5 +1,26 @@
 package Composition;
 
 public class Computer {
+	private Processor processor;
+	private Memory memory;
+	private DisplayCard displayCard;
+	private SoundCard soundCard;
 
+	public Computer() {
+		processor = new Processor("Intel", 8, 3.6, 16);
+		memory = new Memory(16, "DDR4", 3200, "Samsung");
+		displayCard = new DisplayCard(8, "RTX 3060", "Nvidia", 1777);
+		soundCard = new SoundCard(7, 192000, "Creative", "PCIe");
+	}
+
+	public void showInfo() {
+		System.out.println("=== Computer Info ===");
+		processor.printInfo();
+		System.out.println();
+		memory.printInfo();
+		System.out.println();
+		displayCard.printInfo();
+		System.out.println();
+		soundCard.printInfo();
+	}
 }
