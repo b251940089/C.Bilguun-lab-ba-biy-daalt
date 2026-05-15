d5d084b759250bb4029792cd8a545e42ccb86286
b251940089 <b251940089@erdenetis.edu.mn>
2026-05-15 16:42:26 +0800

    Update Memory.java

--- Lab8/src/Composition/Memory.java
+++ Lab8/src/Composition/Memory.java
@@ -1,5 +1,23 @@
 package Composition;
 
 public class Memory {
+	private int size;
+	private String type;
+	private int speed;
+	private String brand;
 
+	public Memory(int size, String type, int speed, String brand) {
+		this.size = size;
+		this.type = type;
+		this.speed = speed;
+		this.brand = brand;
+	}
+
+	public void printInfo() {
+		System.out.println("-- Memory --");
+		System.out.println("Brand: " + brand);
+		System.out.println("Size: " + size + " GB");
+		System.out.println("Type: " + type);
+		System.out.println("Speed: " + speed + " MHz");
+	}
 }
