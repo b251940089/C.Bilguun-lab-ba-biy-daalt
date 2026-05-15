885e993e10a100a3ce1c10d3d81d6bb2a6b9c04c
b251940089 <b251940089@erdenetis.edu.mn>
2026-05-15 16:42:51 +0800

    Update displayCard.java

--- Lab8/src/Composition/displayCard.java
+++ Lab8/src/Composition/displayCard.java
@@ -1,5 +1,23 @@
 package Composition;
 
 public class displayCard {
+	private int memory;
+	private String chipset;
+	private String brand;
+	private int clockSpeed;
 
+	public displayCard(int memory, String chipset, String brand, int clockSpeed) {
+		this.memory = memory;
+		this.chipset = chipset;
+		this.brand = brand;
+		this.clockSpeed = clockSpeed;
+	}
+
+	public void printInfo() {
+		System.out.println("-- Display Card --");
+		System.out.println("Brand: " + brand);
+		System.out.println("Chipset: " + chipset);
+		System.out.println("Memory: " + memory + " GB");
+		System.out.println("Clock Speed: " + clockSpeed + " MHz");
+	}
 }
