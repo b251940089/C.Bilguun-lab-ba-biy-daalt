c5c45d37b95198513d2f46c24d67a87251a724fa
b251940089 <b251940089@erdenetis.edu.mn>
2026-05-15 16:43:12 +0800

    Update processor.java

--- Lab8/src/Composition/processor.java
+++ Lab8/src/Composition/processor.java
@@ -1,5 +1,23 @@
 package Composition;
 
 public class processor {
+	private String brand;
+	private int cores;
+	private double speed;
+	private int cache;
 
+	public processor(String brand, int cores, double speed, int cache) {
+		this.brand = brand;
+		this.cores = cores;
+		this.speed = speed;
+		this.cache = cache;
+	}
+
+	public void printInfo() {
+		System.out.println("-- Processor --");
+		System.out.println("Brand: " + brand);
+		System.out.println("Cores: " + cores);
+		System.out.println("Speed: " + speed + " GHz");
+		System.out.println("Cache: " + cache + " MB");
+	}
 }
